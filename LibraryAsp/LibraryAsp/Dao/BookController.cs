84515d13ed60330ad8f8c57b0b8bd49856df394b
phuongnnhe153074 <90174854+phuongnnhe153074@users.noreply.github.com>
2021-10-29 20:10:34 +0700

    front end borrowbook

--- LibraryAsp/LibraryAsp/Dao/BookController.cs
+++ LibraryAsp/LibraryAsp/Dao/BookController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryAsp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,15 @@ namespace LibraryAsp.Dao
 {
     public class BookController
     {
+        LibraryDbContext myDb = new LibraryDbContext();
+        public List<Book> getAll()
+        {
+            return myDb.books.OrderByDescending(p => p.id_book).ToList();
+        }
+        public void add(Book book)
+        {
+            myDb.books.Add(book);
+            myDb.SaveChanges();
+        }
     }
 }
---
+++ LibraryAsp/LibraryAsp/Dao/BorrowBookDAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryAsp.Dao
+{
+    public class BorrowBookDAO
+    {
+    }
+}
